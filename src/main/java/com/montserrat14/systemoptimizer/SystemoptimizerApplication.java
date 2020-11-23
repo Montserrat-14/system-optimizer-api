d7a31dd5a32c8c2a6a776cf1deb13e774acbd188
Henrique Cabral <henrique.cabral@talkdesk.com>
2020-11-23 20:56:50 +0000

    spring boot setup

---
+++ src/main/java/com/montserrat14/systemoptimizer/SystemoptimizerApplication.java
@@ -0,0 +1,13 @@
+package com.montserrat14.systemoptimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemoptimizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemoptimizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/montserrat14/systemoptimizer/SystemoptimizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.montserrat14.systemoptimizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemoptimizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
